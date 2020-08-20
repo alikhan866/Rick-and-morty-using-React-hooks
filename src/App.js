e01bfe4d23804bf0f8304ef948df558db219b936
Ali Khan <59762820+alikhan866@users.noreply.github.com>
2020-08-20 17:05:46 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,52 +2,87 @@ import React, { useState, useEffect } from 'react';
 import './App.css'
 import Episodes from './components/Episodes/Episodes'
 import CustomNavbar from './components/Navbar/Navbar'
-import Pagination from './components/Pagination/Pagination'
+import { Pagination } from 'antd';
+import 'antd/dist/antd.css';
+
 function App() {
 
   const [postsPerPage] = useState(20);
-  const [currentPage, setCurrentPage] = useState(1);
   const [userSearchValue, setUserSearchValue] = useState('');
-
+  const [totalPosts, setTotalPosts] = useState()
   const [url, setUrl] = useState(``);
   const [urlQuery, setUrlQuery] = useState(``)
+  const [error, setError] = useState(null)
+  const [lastPost, setLastPost] = useState(postsPerPage - 1)
+  const [firstPost, setFirstPost] = useState(lastPost - postsPerPage)
+
+  const [serverCall, setServerCall] = useState(false)
 
   useEffect(() => {
-    setUrl(`https://rickandmortyapi.com/api/episode?page=${currentPage}`)
-  }, [currentPage]);
+    setUrl(`https://rickandmortyapi.com/api/episode/`)
+  }, []);
 
   useEffect(() => {
     setUrlQuery(`https://rickandmortyapi.com/api/episode?name=${userSearchValue}`)
   }, [userSearchValue])
 
-  const paginate = pageNumber => setCurrentPage(pageNumber);
-
   const handleButtonClick = (searchValue) => {
-      setUserSearchValue(searchValue);
+    setUserSearchValue(searchValue);
+  }
+
+  const pageChangeHandler = (pageNumber) => {
+    const indexOfLastPost = (pageNumber * postsPerPage) - 1
+    const indexOfFirstPost = indexOfLastPost - postsPerPage + 1
+    const pagesAtServerCall = []
+    pagesAtServerCall.push((20 / postsPerPage) + 1)
+    for (var i = 0; i < totalPosts / postsPerPage; i++) {
+      pagesAtServerCall.push(pagesAtServerCall[0] + i * (20 / postsPerPage))
+    }
+
+    pagesAtServerCall.map((pageAtServerCall) => {
+      if (pageNumber === pageAtServerCall) {
+        setServerCall(true)
+      }
+    })
+
+    setFirstPost(indexOfFirstPost)
+    setLastPost(indexOfLastPost)
   }
 
   return (
     <div>
       <CustomNavbar
-      onButtonClick={handleButtonClick}
+        onButtonClick={handleButtonClick}
       />
-      <Episodes
-      urlQuery={urlQuery}
-      url={url}
-      />
-    <div
+
+       <Episodes
+          urlQuery={urlQuery}
+          url={url}
+          setTotalPosts={setTotalPosts}
+          firstPost={firstPost}
+          lastPost={lastPost}
+          setUrl={setUrl}
+          serverCall={serverCall}
+          setServerCall={setServerCall}
+          userSearchValue={userSearchValue}
+          setUrlQuery={setUrlQuery}
+          error={error}
+          setError={setError}
+        />
+      <div
         style={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center"
         }}
       >
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={36}
-        paginate={paginate}
-      />
-    </div>
+        {error ? null : <Pagination
+          defaultCurrent={1}
+          total={totalPosts}
+          pageSize={postsPerPage}
+          onChange={pageChangeHandler}
+        />}
+      </div>
     </div>
   );
 }
--- src/components/Episodes/Episodes.js
+++ src/components/Episodes/Episodes.js
@@ -4,52 +4,110 @@ import Episode from './Episode/Episode'
 const Episodes = (props) => {
 
     const [listOfEpisodes, setListOfEpisodes] = useState([])
+    const [nextResponse, setNextResponse] = useState()
+    const [filteredListOfEpisodes, setFilteredListOfEpisodes] = useState([])
+    const { setTotalPosts } = props
+    const { url } = props
+    const { urlQuery } = props
+    const { firstPost } = props
+    const { lastPost } = props
+    const { setUrl } = props
+    const { serverCall } = props
+    const { setServerCall } = props
+    const { userSearchValue } = props
+    const {setUrlQuery} = props
+    const {error} = props
+    const {setError} = props
 
     useEffect(() => {
-        if (props.url) {
-            console.log(props.url)
-            axios.get(props.url)
+        if (serverCall) {
+            setUrl(nextResponse)
+            setServerCall(false)
+        }
+        if (url && userSearchValue == '') {
+            axios.get(url)
                 .then((response) => {
-                    console.log(response)
+                    setError(false)
+                    setNextResponse(response.data.info.next)
+                    setTotalPosts(response.data.info.count)
                     const transformedResponse = response.data.results
-                    setListOfEpisodes(transformedResponse)
+                    setListOfEpisodes([...listOfEpisodes, ...transformedResponse])
                 })
         }
-    },[props.url])
+    }, [url, setTotalPosts, serverCall,userSearchValue])
 
 
-    useEffect(()=>{
-        if (props.urlQuery !== null) {
-            // console.log(props.urlQuery)
-            if (props.urlQuery)
-                axios.get(props.urlQuery)
-                    .then((response) => {
-                        console.log(response)
-                        const transformedResponse = response.data.results
-                        setListOfEpisodes(transformedResponse)
-                    })
+    useEffect(() => {
+        if (serverCall) {
+            setUrlQuery(nextResponse)
+            setServerCall(false)
         }
-    },[props.urlQuery])
+        if (userSearchValue !== '') {
+            if (urlQuery) {
+                axios.get(urlQuery)
+                    .then((response) => {
+                        setError(false)
+                        setNextResponse(response.data.info.next)
+                        setTotalPosts(response.data.info.count)
+                        const transformedResponse = response.data.results
+                        setFilteredListOfEpisodes([...filteredListOfEpisodes, ...transformedResponse])
+                    })
+                .catch((err) => {
+                    setError(true)
+                })
+            }
+        }
+
+    }, [urlQuery, setTotalPosts, serverCall])
+
+    let episodes
+
+    if(userSearchValue === '') {
+        episodes = (
+            <div>
+                {listOfEpisodes ? listOfEpisodes.map((resp, index) => {
+                    while (index >= firstPost && index <= lastPost) {
+                        return (
+                            <Episode
+                                episodeName={resp.name}
+                                id={resp.id}
+                                url={resp.url}
+                                episodeDate={resp.air_date}
+                                key={resp.id}
+                            />
+                        )
+                    }
+                }) : null}
+            </div>
+        )
+    } else {
+        episodes = (
+            <div>
+                {filteredListOfEpisodes ? filteredListOfEpisodes.map((resp, index) => {
+                    while (index >= firstPost && index <= lastPost) {
+                        return (
+                            <Episode
+                                episodeName={resp.name}
+                                id={resp.id}
+                                url={resp.url}
+                                episodeDate={resp.air_date}
+                                key={resp.id}
+                            />
+                        )
+                    }
+                }) : null}
+            </div>
+        )
+    }
 
 
-    const episodes = (
-        <div>
-            {listOfEpisodes.map((resp) => {
-                return (
-                    <Episode
-                        episodeName={resp.name}
-                        id={resp.id}
-                        url={resp.url}
-                        episodeDate={resp.air_date}
-                        key={resp.id}
-                    />
-                )
-            })}
-        </div>
-    )
     return (
         <div>
-            {episodes}
+            {error
+            ?<h2>
+                No such episode found please enter a valid episode name
+            </h2>
+            :episodes}
         </div>
     )
 }
